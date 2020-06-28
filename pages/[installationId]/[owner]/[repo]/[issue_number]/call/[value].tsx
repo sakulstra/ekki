6bc7a4a0419fcff5d1e2c2c4588b8f3df24b1f59
Jonas Strassel <jonas.strassel@mobilehead.com>
2020-06-28 22:21:34 +0200

    fix: react is sooo ugly

--- pages/[installationId]/[owner]/[repo]/[issue_number]/call/[value].tsx
+++ pages/[installationId]/[owner]/[repo]/[issue_number]/call/[value].tsx
@@ -4,19 +4,23 @@ import { ClientInput } from '@utils/handler/types'
 
 export async function getServerSideProps({ req, params }) {
   const session = await getSession({ req })
-  const data = await fetch(
-    `${process.env.VERCEL_URL || process.env.BASE_URL}/api/app-webhooks`,
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        type: APP_EVENTS.pokerCall,
-        params: {
-          ...params,
-          userId: session.user.id,
-        },
-      } as ClientInput),
-    }
-  ).then((res) => res.json())
+  let data = {}
+  if (session) {
+    data = await fetch(
+      `${process.env.VERCEL_URL || process.env.BASE_URL}/api/app-webhooks`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          type: APP_EVENTS.pokerCall,
+          params: {
+            ...params,
+            userId: session.user.id,
+          },
+        } as ClientInput),
+      }
+    ).then((res) => res.json())
+  }
+
   return {
     props: { data }, // will be passed to the page component as props
   }
@@ -26,13 +30,18 @@ const Post = ({ data }) => {
   const [session, loading] = useSession()
   if (!session && !loading) signin('github')
 
-  if (data?.nextUrl && typeof window !== 'undefined') {
-    window.location.href = data.nextUrl
+  const redirect = () => {
+    if (data?.nextUrl && typeof window !== 'undefined') {
+      window.location.href = data.nextUrl
+    }
+    return true
   }
 
   return (
     <>
-      {session?.user && <p>Thank you for voting {session.user.email}</p>}
+      {session?.user && redirect && (
+        <p>Thank you for voting {session.user.email}</p>
+      )}
       {!session && <p>Signing in...</p>}
     </>
   )
